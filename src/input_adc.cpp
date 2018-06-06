1071f26d4058f92723f107dae582e812e44db194
Luke Shimanuki <luke.shimanuki@gmail.com>
2018-06-05 22:20:39 -0700

    uc actually respects sampling_rate

--- src/input_adc.cpp
+++ src/input_adc.cpp
@@ -4,12 +4,28 @@
 
 #include "global.hpp"
 
+static uint64_t prev_time = 0;
+
 void init_input() {
+	prev_time = micros();
 }
 
-float in[num_channels];
+static float in[num_channels];
 
 const float* input() {
+	while (true) {
+		const uint64_t time = micros();
+
+		// continue at next tick
+		if (time - prev_time >= 1000000 / sampling_rate) {
+			prev_time = time;
+			break;
+		}
+
+		// handle overflow
+		if (time < prev_time) prev_time = time;
+	}
+
 	for (size_t i = 0; i < num_channels; ++i)
 		in[i] = float(analogRead(A0 + i)) / 1024;
 
