94e899bb1169aada4031ba9df6a43a2ecacd25e0
Luke Shimanuki <luke.shimanuki@gmail.com>
2018-06-18 23:29:55 -0700

    use interval timer

--- include/global.hpp
+++ include/global.hpp
@@ -1,6 +1,6 @@
 #include <stddef.h>
 
-constexpr const uint64_t sampling_rate = 94000;
-constexpr const size_t num_channels = 2;
+constexpr const uint64_t sampling_rate = 100000;
+constexpr const size_t num_channels = 4;
 constexpr const size_t block_size = 32;
 
--- src/input_adc.cpp
+++ src/input_adc.cpp
@@ -6,18 +6,15 @@
 #include "global.hpp"
 
 static ADC* adc = new ADC();
-
-static constexpr const ADC_SAMPLING_SPEED SPEED =
-	sampling_rate == 94000 ? ADC_SAMPLING_SPEED::LOW_SPEED :
-	sampling_rate == 231000 ? ADC_SAMPLING_SPEED::MED_SPEED :
-	ADC_SAMPLING_SPEED::HIGH_SPEED
-;
+static IntervalTimer timer;
 
 // pin[0] = A0 so it can be used by ADC_0
 // pin[1] = A14 so it can be used by ADC_1
 // A2 and A3 can be used by either ADC
 const int base_pins[] = {A0, A14, A2, A3};
 int pin[num_channels];
+
+void timer_callback();
 
 void init_input() {
 	for (size_t i = 0; i < sizeof(base_pins) / sizeof(int) && i < num_channels; ++i)
@@ -29,24 +26,27 @@ void init_input() {
 	for (size_t i = 0; i < num_channels; ++i)
 		pinMode(pin[i], INPUT);
 
+	delay(100);
+
 	if (num_channels >= 1) {
 		adc->setAveraging(1);
 		adc->setResolution(12);
 		adc->setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED);
-		adc->setSamplingSpeed(SPEED);
+		adc->setSamplingSpeed(ADC_SAMPLING_SPEED::HIGH_SPEED);
 		adc->enableInterrupts(ADC_0);
-		adc->startContinuous(pin[0], ADC_0);
 	}
 
 	if (num_channels >= 2) {
 		adc->setAveraging(1, ADC_1);
 		adc->setResolution(12, ADC_1);
 		adc->setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED, ADC_1);
-		adc->setSamplingSpeed(SPEED, ADC_1);
-		adc->startContinuous(pin[1], ADC_1);
+		adc->setSamplingSpeed(ADC_SAMPLING_SPEED::HIGH_SPEED, ADC_1);
+		adc->enableInterrupts(ADC_1);
 	}
 
-	delay(500);
+    timer.begin(timer_callback, int32_t(1000000 / sampling_rate));
+
+	delay(100);
 }
 
 static int16_t buffer[2][num_channels][block_size] __attribute__ ((aligned (128)));
@@ -62,30 +62,39 @@ int16_t (*input())[block_size] {
 	return buffer[1 - which_buffer];
 }
 
+void take_measurement(uint8_t);
+
+void timer_callback() {
+	take_measurement(0);
+	take_measurement(1);
+}
+
+static uint8_t curr_channel[2] = {};
+
+void take_measurement(uint8_t channel) {
+	if (channel == num_channels) {
+		++buffer_idx;
+		if (buffer_idx >= block_size) {
+			has_data = true;
+			buffer_idx = 0;
+			which_buffer = 1 - which_buffer;
+		}
+	}
+	if (channel >= num_channels) return;
+
+	adc->startSingleRead(pin[channel], channel % 2 ? ADC_1 : ADC_0);
+	curr_channel[channel % 2] = channel;
+}
+
 void adc0_isr() {
-	if (num_channels >= 1) buffer[which_buffer][0][buffer_idx] = adc->analogReadContinuous(ADC_0);
-	if (num_channels >= 2) buffer[which_buffer][1][buffer_idx] = adc->analogReadContinuous(ADC_1);
-	// TODO: do something with adc->isComplete(ADC_0)
-	if (num_channels == 3) {
-		buffer[which_buffer][2][buffer_idx] = adc->analogRead(pin[2]);
-	} else if (num_channels == 4) {
-		adc->startSingleRead(pin[2], ADC_0);
-		buffer[which_buffer][3][buffer_idx] = adc->analogRead(pin[3], ADC_1);
-		buffer[which_buffer][2][buffer_idx] = adc->readSingle(ADC_0);
-	}
-	for (size_t i = 4; i < num_channels; ++i)
-		buffer[which_buffer][i][buffer_idx] = adc->analogRead(pin[i]);
-
-	++buffer_idx;
-
-	if (buffer_idx >= block_size) {
-		has_data = true;
-
-		buffer_idx = 0;
-		which_buffer = 1 - which_buffer;
-	}
+	const uint8_t channel = curr_channel[0];
+	buffer[which_buffer][channel][buffer_idx] = adc->analogReadContinuous(ADC_0);
+	take_measurement(channel + 2);
 }
 
 void adc1_isr() {
+	const uint8_t channel = curr_channel[1];
+	buffer[which_buffer][channel][buffer_idx] = adc->analogReadContinuous(ADC_1);
+	take_measurement(channel + 2);
 }
 
